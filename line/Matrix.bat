8065c6e8d2b4e692b71970c7f698d00791390091
mohammad654 <mohammad.ali.shikhi.55@gmail.com>
2022-05-28 05:53:07 +0200

    first commit

---
+++ line/Matrix.bat
@@ -0,0 +1,15 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+gotogreeting 
+:Greeting 
+cls
+echo Mohammad Ali 
+pause
+cls
+goto Matrix
+:Matrix
+echo %random% Mohammad Ali %random% Mohammad Ali %random% Mohammad Ali %random% Mohammad Ali %random% Mohammad Ali %random% Mohammad Ali %random% Mohammad Ali %random% Mohammad Ali 
+PING 1.1.1.1 -n 1-W 5 >NUL
+goto Matrix
---
+++ line/mo.bat
@@ -0,0 +1,13 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+gotogreeting 
+:Greeting 
+echo Hello Mohammad Ali 
+pause
+cls
+goto Matrix
+:Matrix
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%%random% %random% %random% %random% %random% %random% %random%%random%%random%%random% %random% %random% %random% %random% %random%%random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto Matrix
